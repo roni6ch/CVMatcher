bd15a589e3ec6d7588ecc920e80deab289c6d9bd
roni6ch <roni6ch@gmail.com>
2016-02-27 17:53:32 +0200

    יעילות + קורות חיים ועוד

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 /** **********************************ANGULAR*************************************** */
 
-var app = angular.module('cvmatcherApp', [ "ngRoute"  ,'infinite-scroll']);
+var app = angular.module('cvmatcherApp', [ "ngRoute", 'infinite-scroll' ]);
 
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
@@ -15,6 +15,9 @@ app.config(function($routeProvider) {
 	}).when('/favorites', {
 		templateUrl : 'favorites.html',
 		controller : 'favoritesController'
+	}).when('/resume/:name', {
+		templateUrl : 'resume.html',
+		controller : 'resumeController'
 	})
 });
 
@@ -22,85 +25,69 @@ app.config(function($routeProvider) {
  * ********************* myjobs controller ****************
  */
 
-app.controller('myjobsController', function($scope,$http) {
+app.controller('myjobsController', function($scope, $http) {
 });
 
 /*
  * ********************* readcvs controller ****************
  */
-app.controller('readcvsController', function($scope,$http) {
-	
+app.controller('readcvsController', function($scope, $http) {
 
 });
 /*
  * ********************* unreadcvs controller ****************
  */
-app.controller('unreadcvsController', function($scope,$http) {
-	
+app.controller('unreadcvsController', function($scope, $http) {
+
 });
 
 /*
  * ********************* favorites controller ****************
  */
-app.controller('favoritesController', function($scope,$http) {
-	$scope.getMainJson = function(){
-	$http.get("json/myjobs.json").success( function(response) {
-	      $scope.jobs = response; 
-	   });
+app.controller('favoritesController', function($scope, $http) {
+	$scope.getMainJson = function() {
+		$http.get("json/myjobs.json").success(function() {
+		}).success(function(data, status, headers, config) {
+			$scope.jobs = data
+			console.log(data);
+	    }).error(function(data, status, headers, config) {
+	        alert("myjobs AJAX failed!");
+	    });
 	}
 
+});
+/*
+ * ********************* resume controller ****************
+ */
+app.controller('resumeController', function($scope, $http,$location) {
+	var path = $location.path().split('/')[2];
+	$http.get("json/resume.json").success(function() {
+	}).success(function(data, status, headers, config) {
+		$scope.user = data[path][0];
+    }).error(function(data, status, headers, config) {
+        alert("users AJAX failed!");
+    });
 });
 
 
 /*
  * ********************* DIRECTIVES ****************
  */
-app.directive("compileHome", function($compile){
-    return{
-        link: function(scope, element){
-            element.html($compile("<a href='#'>Homepage</a>")(scope));
-        }
-    }
-});
-app.directive("compileReadCvs", function($compile){
-    return{
-        link: function(scope, element){
-            element.html($compile("<a href='#'>Homepage</a><span> > </span><a href='#'>ReadCv's</a>' ")(scope));
-        }
-    }
-});
-app.directive("compileUnreadCvs", function($compile){
-    return{
-        link: function(scope, element){
-            element.html($compile("<a href='#'>Homepage</a><span> > </span><a href='#'>UnreadCv's</a>' ")(scope));
-        }
-    }
-});
-app.directive("compileFavorites", function($compile){
-    return{
-        link: function(scope, element){
-            element.html($compile("<a href='#'>Homepage</a><span> > </span><a href='#'>Favorites</a>' ")(scope));
-        }
-    }
-});
-
-
-
-//background: url("../img/ProfileImg.png") no-repeat;
-/*app.directive('backImg', function() {
-return function(scope, element, attrs) {
-attrs.$observe('backImg', function(value) {
-element.css({
-'background-image' : 'url(uploads/' + $.trim(value) + ')',
-'background-size' : 'cover'
-});
-});
-};
-});*/
-
-/*$("input").keypress(function(e) {
-	if (e.which == 13 && $(this).hasClass('input') ) {
-		$scope.loginSystem();
-		return false;
+app.directive("compileHtml", function($compile, $location) {
+	return {
+		link : function(scope, element) {
+			var path = $location.path().split('/');
+			if (path[1] != "")
+				path = "<span> > </span><a href='#/" + path[1] + "'>" + path[1]
+						+ "</a>";
+			else
+				path = "";
+			element.html($compile("<a href='#/'>Homepage</a>" + path)(scope));
+		}
 	}
-});	*/
+});
+
+/*
+ * $("input").keypress(function(e) { if (e.which == 13 &&
+ * $(this).hasClass('input') ) { $scope.loginSystem(); return false; } });
+ */
