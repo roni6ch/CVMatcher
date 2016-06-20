f176a63ae35cf0261adb5f82280c07ab0dd2cf41
SuperMarket-Project <roni6ch@gmail.com>
2016-06-21 00:01:20 +0300

    HIRE VS FIRE

--- js/controllers/employer/hired.js
+++ js/controllers/employer/hired.js
@@ -8,9 +8,12 @@
 
 app.controller('hiredController', function ($rootScope, $location, $scope, $http) {
 
+
     $scope.init = function () {
         $(".navigation")[0].innerHTML = "<a href='#/login'>Homepage</a><span> > </span><a href='#/hired'>Hired</a>";
         angular.element(".fa-pulse").show();
+
+
         $http({
             url: 'https://cvmatcher.herokuapp.com/employer/getEmployees',
             method: "POST",
@@ -27,5 +30,34 @@ app.controller('hiredController', function ($rootScope, $location, $scope, $http
                     console.log("getEmployees AJAX failed!");
                     console.log(response);
                 });
+    }
+
+    $scope.fired = function(indx,id){
+        $("#indx-"+indx+">.fired").css("background","url('images/fired1.png')");
+        $("#indx-"+indx+">.hiredImg").css("background","url('images/hired2.png')");
+
+
+        $http({
+            url: 'https://cvmatcher.herokuapp.com/employer/setDecision',
+            method: "POST",
+            data: {
+                "personal_properties_id":id,
+                "decision": false
+            }
+        }).then(function (data) {
+            });
+    }
+    $scope.hired = function(indx,id){
+        $("#indx-"+indx+">.hiredImg").css("background","url('images/hired1.png')");
+        $("#indx-"+indx+">.fired").css("background","url('images/fired2.png')");
+        $http({
+            url: 'https://cvmatcher.herokuapp.com/employer/setDecision',
+            method: "POST",
+            data: {
+                "personal_properties_id":id,
+                "decision": true
+            }
+        }).then(function (data) {
+        });
     }
 });
